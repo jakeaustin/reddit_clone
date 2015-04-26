735d0f346b3b0b5450aea5fa0234f2a7988f3489
Jake Austin <jakeaust07@gmail.com>
2015-04-26 12:38:42 -0400

    connect mainController to firebase, make test post

--- public/controllers/mainController.js
+++ public/controllers/mainController.js
@@ -1,5 +1,15 @@
 (function() {
-  angular.module('reddit').controller('mainController', function($scope) {
-    $scope.test = 'foo';
-  });
+  angular.module('reddit')
+    .controller('mainController', function($scope, $firebase, FIREBASE_URI) {
+      var ref = new Firebase(FIREBASE_URI);
+      var posts = $firebase(ref).$asArray();
+
+      $scope.test = 'foo';
+      $scope.posts = posts;
+      posts.$add({
+        name: "TIFU by making reddit",
+        description: "^title....and this whole site",
+        url: "https://www.reddit.com"
+      });
+    });
 })();
