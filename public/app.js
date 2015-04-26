aa2bcc6efd11846ea401e6b84bab876efc6782c5
Jake Austin <jakeaust07@gmail.com>
2015-04-26 12:27:16 -0400

    define firebase constant

--- public/app.js
+++ public/app.js
@@ -1,9 +1,11 @@
 (function() {
-	var app = angular.module('reddit', ['ngRoute',
-                            'firebase',
-                            'ngCookies',
-                            'LocalStorageModule'
-                            ]);
+  var app = angular.module('reddit', ['ngRoute',
+                          'firebase',
+                          'ngCookies',
+                          'LocalStorageModule'
+                          ]);
+
+  app.constant('FIREBASE_URI', 'https://jaustin-reddit.firebaseio.com/');
 
   app.config(function($routeProvider) {
     $routeProvider
