1d43445d8154ea7160b69636bbabdeb53264c6b3
Jake Austin <jakeaust07@gmail.com>
2015-04-26 12:14:52 -0400

    defined routes

--- public/app.js
+++ public/app.js
@@ -1,7 +1,17 @@
 (function() {
-	angular.module('reddit', ['ngRoute',
+	var app = angular.module('reddit', ['ngRoute',
                             'firebase',
                             'ngCookies',
                             'LocalStorageModule'
                             ]);
+
+  app.config(function($routeProvider) {
+    $routeProvider
+    .when('/', {
+      controller: 'mainController',
+      templateUrl:'/views/main.html'
+    })
+    .otherwise({redirectTo: '/'});
+  });
+
 })();
