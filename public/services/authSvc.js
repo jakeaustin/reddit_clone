2ef8b3a7b2471e8cbb2d03c4bafe9c5d40366cdf
Jake Austin <jakeaust07@gmail.com>
2015-05-02 12:32:51 -0400

    defined auth service with register/login/logout/resolve. Login appears to work but logs an error. Logout wipes the dom for some reason

---
+++ public/services/authSvc.js
@@ -0,0 +1,37 @@
+(function() {
+  angular.module('reddit')
+  .factory('Auth', function ($firebaseAuth, FIREBASE_URI, $rootScope) {
+    var ref = new Firebase(FIREBASE_URI);
+    var auth = $firebaseAuth(ref);
+
+    var Auth = {
+      register: function (user) {
+        console.log("create user");
+        return auth.$createUser(user.email, user.password);
+      },
+      //getting an error in the .then, but it appears to be working...
+      login: function (user) {
+        console.log('login');
+        return auth.$authWithPassword({
+                                      email:user.email,
+                                      password:user.password
+                                    }).then(function(error, userData) {
+                                      if(error) {
+                                        console.log("error logging in :" + userData);
+                                      }
+                                      else {
+                                        console.log("log in successful");
+                                        $rootScope.$broadcast('$firebaseAuth:authWithPassword', userData);
+                                      }
+                                  });
+      },
+      logout: function () {
+        auth.$unauth();
+      },
+      resolveUser: function() {
+        return auth.$getAuth();
+      },
+    };
+    return Auth;
+  });
+})();
