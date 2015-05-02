181b9a4a4820f1dcaf44a8c7763898bf0b13f5db
Jake Austin <jakeaust07@gmail.com>
2015-05-02 12:35:38 -0400

    inject auth service and user, replace simple signup with auth service

--- public/controllers/registerController.js
+++ public/controllers/registerController.js
@@ -1,40 +1,17 @@
 (function() {
   angular.module('reddit')
-  .controller('registerController', function($scope, $firebase, Posts, FIREBASE_URI, $window, localStorageService) {
+  .controller('registerController', function($scope, Auth, user, $window) {
+    if (user) {
+      $window.location.href = '/';
+    }
 
-    $scope.login = function(email, pwd) {
-      var ref = new Firebase(FIREBASE_URI);
-
-      ref.authWithPassword({
-        email: email,
-        password: pwd
-      }, function(error, userData) {
-        if(error) {
-          console.log("Error logging in: " + error);
-        }
-        else {
-          console.log("Login Successful: " + userData);
-          localStorageService.set('authData', userData);
-          $window.location.href= '/';
-        }
-      });
-    };
-
-    $scope.signup = function(email, pwd) {
-      var ref = new Firebase(FIREBASE_URI);
-
-      ref.createUser({
-        email: email,
-        password: pwd
-      }, function(error, userData) {
-        if(error) {
-          console.log("Error creating user: " + error);
-        }
-        else {
-          console.log("Successfully created user with uid: " + userData.uid);
-          localStorageService.set('authData', userData);
-          $window.location.href = '/';
-        }
+    $scope.register = function() {
+      Auth.register($scope.user).then(function() {
+        return Auth.login($scope.user).then(function() {
+          $window.location.href='/';
+        });
+      }, function(error) {
+        $scope.error = error.toString();
       });
     };
   });
